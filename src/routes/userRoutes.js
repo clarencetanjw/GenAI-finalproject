808aae64b7030aafc0c6e292886bdd51f2526474
clarencetanjw <clarencetan91@gmail.com>
2024-09-03 01:20:03 -0400

    Final changes to Github with Dockerfile

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+// Route for user registration
 router.post('/register', userController.registerUser);
+
+// Route for user login
 router.post('/login', userController.loginUser);
-router.put('/:username', userController.updateUserProfile);
+
+// Route for updating user profile
+router.patch('/:username', userController.updateUserProfile);
 
 module.exports = router;
